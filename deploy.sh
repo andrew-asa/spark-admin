0c30bb49b873da3290922f2b33ad2e9bf0b7fb33
andrew.asa <andrew.asa@fanruan.com>
2018-10-25 14:57:05 +0800

    修改仓库地址

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
-dst=".."
-mvn install && cp target/spark-admin-1.0-SNAPSHOT.jar "$dst/lib/spark-admin-1.0-SNAPSHOT.jar" && cp target/spark-admin-1.0-SNAPSHOT-sources.jar "$dst/lib/spark-admin-1.0-SNAPSHOT-sources.jar"
+# dst=".."
+# mvn install && cp target/spark-admin-1.0-SNAPSHOT.jar "$dst/lib/spark-admin-1.0-SNAPSHOT.jar" && cp target/spark-admin-1.0-SNAPSHOT-sources.jar "$dst/lib/spark-admin-1.0-SNAPSHOT-sources.jar"
+mvn deploy -DaltDeploymentRepository=andrew.asa-maven-repository::default::file:/Users/andrew_asa/Documents/code/github/andrew-asa/maven-repository/
