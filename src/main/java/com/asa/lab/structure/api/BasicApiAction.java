8a6e3fa73b865295d0a8078b055d2598dd780d5d
andrew.asa <andrew.asa@fanruan.com>
2019-01-14 10:43:54 +0800

    相关配置

---
+++ src/main/java/com/asa/lab/structure/api/BasicApiAction.java
@@ -0,0 +1,10 @@
+package com.asa.lab.structure.api;
+
+/**
+ * @author andrew_asa
+ * @date 2018/11/23.
+ * 对外开放api 请求
+ */
+public class BasicApiAction {
+
+}
---
+++ src/main/java/com/asa/lab/structure/api/ConfigureApiAction.java
@@ -0,0 +1,10 @@
+package com.asa.lab.structure.api;
+
+/**
+ * @author andrew_asa
+ * @date 2018/11/23.
+ * api
+ */
+public class ConfigureApiAction extends BasicApiAction {
+
+}
